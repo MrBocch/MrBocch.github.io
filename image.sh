5b62f6ab9306fd86fe1f0630ad99c910fe48ef86
MrBocch <jorgealberto1436@gmail.com>
2024-08-12 13:24:14 -0600

    simplest solution i thought of, but it feels wrong

--- image.sh
+++ image.sh
@@ -4,28 +4,30 @@ _image_count(){
     count=$(ls -l static/images/ | wc -l )
     # why cant i do it in 1 line?
     count=$((count - 1))
-    echo $(($count))
+    echo $count
 }
 
 # i could probably use seq
 # seq [start] [increment] [limit]
 # an interesting idea but i=0; i++; is simpler
+
+# ill make it really simple
+# i will paste the images to the staging area to rename them
+# then paste them into images
 _rename_images(){
-    icount=_image_count
-    i=0
-    echo $icount
-    for image in static/images/*; do
-        if [ -e "static/images/${i}.jpg" ]; then
-            echo "File ${i}.jpg exists. dont overwrite"
-            i=$((i+1))
-        else
-            echo "file ${i}.jpg not exist"
-            i=$((i+1))
-        fi
-        #echo "renaming $image to $i.jpg"
-        #mv $image "static/images/$i.jpg"
-        #i=$((i+1))
+    local icount=$(_image_count)
+    i=$icount
+    for image in static/stage/* ; do
+        echo "renaming $image to ${i}.jpg"
+        mv $image "static/stage/${i}.jpg"
+        i=$((i+1))
     done
+    echo "moving staging images to images folder"
+    echo "dont forget to update the js"
+    # maybe i can do that in here also, just write to a script.js
+    mv static/stage/* static/images/
+    _remove_metadata
+    _remove_metadata
 }
 
 _remove_metadata(){
