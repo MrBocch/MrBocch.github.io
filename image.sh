7fb284704f82fe98f1ee2769309fb4665e5b03a7
MrBocch <jorgealberto1436@gmail.com>
2024-08-16 16:17:44 -0600

    i figured it out, wheen feeding for image in ./static/stage/test.png ./static/stage/1721066250795347.png the awk script is taking (.)/static---- as tthe things woops lol

--- image.sh
+++ image.sh
@@ -1,62 +1,19 @@
 # /bin/zsh
 
-_image_count(){
-    count=$(ls -l static/images/ | wc -l )
-    # why cant i do it in 1 line?
-    count=$((count - 1))
-    echo $count
-}
-
-# it does alot more than jus rename them
-# but i think its ok, just rename it
-_rename_images(){
-    local icount=$(_image_count)
-    i=$icount
-    for image in static/stage/*.jpg ; do
-        echo "renaming $image to ${i}.jpg"
-        mv $image "static/stage/${i}.jpg"
-        i=$((i+1))
-    done
-    echo "moving staging images to images folder"
-    mv static/stage/*.jpg static/images/
-    _remove_metadata
-    _update_script
+_get_name(){
+    echo $1 | awk '{ split($0, arr, "."); print arr[1] }'
 }
 
 _get_extension(){
     echo $1 | awk '{ split($0, arr, "."); print arr[2] }'
 }
 
-_get_name(){
-    echo $1 | awk '{ split($0, arr, "."); print arr[1] }'
+_image_count(){
+    count=$(ls -l static/images/ | wc -l )
+    # why cant i do it in 1 line?
+    count=$((count - 1))
+    echo $count
 }
-
-_convert_to_jpg(){
-    local image=$1
-    # why does this not work anymore? is there something wrong with the function?
-    # calling the function here seems to not work?
-    # name="$(_get_name $image)"
-    # ext="$(_get_extension $image)"
-    if [ $ext == "jpg" ]; then
-        echo "this $image is a .jpg"
-    else
-        # it works with png but can it do other formats?
-        echo "converting $image to a .jpg"
-        # ffmpeg -i $image $name.jpg
-    fi
-}
-
-# _convert_to_jpg static/stage/test.png
-# ffmpeg does not destroy the original image so what should i do? not movethem or delete them?
-_convert_staging(){
-    for image in $(find ./static/stage -type f ! -name "*.jpg") ; do
-        echo "Will try to convert $image to .jpg"
-        _convert_to_jpg $image
-    done
-}
-
-_get_name "hello.jpg"
-_get_extension "hello.jpg"
 
 _remove_metadata(){
     for image in static/images/*; do
@@ -77,6 +34,47 @@ document.getElementById("image").src = \`\${path}\${imageIndex}.jpg\`;
 EOF
     echo "done!"
 }
+
+# it does alot more than jus rename them
+# but i think its ok, just rename the function
+_rename_images(){
+    local icount=$(_image_count)
+    i=$icount
+    for image in static/stage/*.jpg ; do
+        echo "renaming $image to ${i}.jpg"
+        mv $image "static/stage/${i}.jpg"
+        i=$((i+1))
+    done
+    echo "moving staging images to images folder"
+    mv static/stage/*.jpg static/images/
+    _remove_metadata
+    _update_script
+}
+
+# its ok, just take it step by step
+
+_convert_to_jpg(){
+    local image=$1
+    echo $image # works as expected
+    # I KNOW WHY IT DOES NOT WORK
+    # awk is
+    # ./static/stage/img.png
+    # its taking the first one right?
+    echo $(_get_name $image)
+    echo $(_get_extension $image)
+    #name="$(_get_name $image)"
+    #ext="$(_get_extension $image)"
+    # ffmpeg -i $image $name.jpg
+}
+_convert_staging(){
+    # go thrue each not .jpg file, and will call _convert_to_jpg() on them
+    for image in $(find ./static/stage -type f ! -name "*.jpg") ; do
+        echo "Will try to convert $image to jpg"
+        _convert_to_jpg $image
+    done
+}
+
+_convert_staging
 
 _help(){
 	echo "  image.sh remove : removes metadata from images"
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const path = "static/images/";
-const imageCount = 12;
+const imageCount = 10;
 const imageIndex = Math.floor(Math.random() * imageCount);
 document.getElementById("image").src = `${path}${imageIndex}.jpg`;
