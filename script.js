82f8f4ce4a4329d84f54f819402a5ae24e29bee8
MrBocch <jorgealberto1436@gmail.com>
2024-08-30 13:39:01 -0600

    new batch

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const path = "static/images/";
-const imageCount = 25;
+const imageCount = 32;
 const imageIndex = Math.floor(Math.random() * imageCount);
 document.getElementById("image").src = `${path}${imageIndex}.jpg`;
