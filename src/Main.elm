72ef2af2ed0f74fa90b0ffe2e753e46b632dad4a
MrBocch <jorgealberto1436@gmail.com>
2026-09-18 10:18:23 -0600

    update

--- src/Main.elm
+++ src/Main.elm
@@ -1,24 +1,25 @@
 module Main exposing (main)
 
-import Html exposing (Html, div, h1, p, a, ul, li, text)
+import Html exposing (Html, a, div, h1, li, p, text, ul)
 import Html.Attributes exposing (class, href, target)
 
-main = view
+
+main =
+    view
+
 
 view : Html msg
 view =
     div [ class "hero" ]
-        [
-            h1 []
-                [ text "JORGE.SH" ],
-            p []
-                [ text "Currently in my 6th semester in ",
-                  a [ href "https://www.fime.uanl.mx/", target "_blank"] [text "FIME"],
-                  text ". On my free time I like to hike."
-                ],
-            ul []
-                [
-                  li [] [ a [ href "https://www.github.com/MrBocch" ] [text "~> Github"]],
-                  li [] [ a [ href "https://www.jorge.sh/blog" ] [text "~> Blog"]]
-                ]
+        [ h1 []
+            [ text "JORGE.SH" ]
+        , p []
+            [ text "Currently in my 8th semester in "
+            , a [ href "https://www.fime.uanl.mx/", target "_blank" ] [ text "FIME" ]
+            , text ". On my free time I like to hike."
+            ]
+        , ul []
+            [ li [] [ a [ href "https://www.github.com/MrBocch" ] [ text "~> Github" ] ]
+            , li [] [ a [ href "https://www.youtube.com/watch?v=aMyVyR64urY" ] [ text "~> Blog" ] ]
+            ]
         ]
